6f7884dd9e28cce6301c1cbc6c1a1e2c9ce5e616
FatimaKarimova28 <fatimaika@code.edu.az>
2023-01-21 01:46:04 +0400

    done again

--- Two-dimensional array/Program.cs
+++ Two-dimensional array/Program.cs
@@ -11,9 +11,28 @@
 //}
 //Console.WriteLine(sum);
 
-
-
-
+int number;
+int sum = 0;
+int[,] arr = new int[3, 3]
+{
+    {1, 2, 3},
+    {4, 5, 6 },
+    {7, 8, 9 }
+};
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        if (i == j)
+        {
+            sum += arr[i, j];   
+        }
+     
+      
+    }
+  
+}
+Console.WriteLine(sum);
 
 //using System.Threading.Channels;
 
@@ -48,24 +67,24 @@
 
 
 
-int[,] arr = new int[5, 5]
-{
-    {1, 2, 3, 4, 5 },
-    {6, 7, 8, 9, 10 },
-    {11, 12, 13, 14, 15 },
-    {16, 17, 18, 19, 20 },
-    {21, 22, 23, 24, 25 }
-};
-for (int i = 0; i < arr.GetLength(0); i++)
-{
-    for (int j = 0; j < arr.GetLength(1); j++)
-    {
-        if (i > 3 - j)
-        {
-            arr[i, j] = 0;
-        }
-        Console.Write(arr[i, j] + " ");
+//int[,] arr = new int[5, 5]
+//{
+//    {1, 2, 3, 4, 5 },
+//    {6, 7, 8, 9, 10 },
+//    {11, 12, 13, 14, 15 },
+//    {16, 17, 18, 19, 20 },
+//    {21, 22, 23, 24, 25 }
+//};
+//for (int i = 0; i < arr.GetLength(0); i++)
+//{
+//    for (int j = 0; j < arr.GetLength(1); j++)
+//    {
+//        if (i > 3 - j)
+//        {
+//            arr[i, j] = 0;
+//        }
+//        Console.Write(arr[i, j] + " ");
 
-    }
-    Console.WriteLine();
-}
+//    }
+//    Console.WriteLine();
+//}
